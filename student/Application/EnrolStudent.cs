ee75d382ccde525ebc5e08807420b58246f82fee
Marilag Svennevig <md@dewise.com>
2022-09-25 17:09:16 +0200

    Initial commit - 3 services, non-containerized

---
+++ student/Application/EnrolStudent.cs
@@ -0,0 +1,16 @@
+namespace student
+{
+public class EnrolStudent
+{
+    public string? FirstName { get; set; } 
+    public string? LastName { get; set; }
+    public string? Address { get; set; }
+    public string? Email { get; set; }
+    public int SchoolYear { get; set; }
+    public string? Term { get; set; }
+    public string? Program { get; set; }
+    
+
+}
+}
+
